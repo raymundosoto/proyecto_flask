e10cc9a3fcf371ba8db5e5ad4e1aca7df2eb7532
Raymundo Soto <72757419+raymundosoto@users.noreply.github.com>
2023-12-20 14:58:51 -0600

    Update app.py
    Cambio menor

--- app.py
+++ app.py
@@ -4,4 +4,4 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return '<h1>Hola mundo</h1><p>Desde Flask</p>'
+    return '<h1>Hola mundo</h1><p>Desde Flask</p><p>Aprendiendo web apps</p> '
